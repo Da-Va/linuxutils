e4ee8e8d7a73ea81685bb17865b9715b8aed9db8
Da-Va <david1valouch@gmail.com>
2022-02-02 19:53:15 +0100

    shellcheck changes

--- system-init/dotfiles-init.sh
+++ system-init/dotfiles-init.sh
@@ -5,17 +5,17 @@
 REMOTE=git@github.com:Da-Va/dotfiles.git
 LOCAL=$HOME/.dotfiles
 
-git clone --bare $REMOTE $LOCAL
-function config {
-   /usr/bin/git --git-dir=$LOCAL/ --work-tree=$HOME $@
+git clone --bare "$REMOTE" "$LOCAL"
+config() {
+  /usr/bin/git --git-dir="$LOCAL"/ --work-tree="$HOME" "$@"
 }
 config checkout
 if [ $? = 0 ]; then
-    echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    mkdir -p .config-backup
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+  mkdir -p .config-backup
+  config checkout 2>&1 | grep -E "\s+\." | awk \{'print $1'\} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
