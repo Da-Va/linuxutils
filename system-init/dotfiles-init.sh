eedd8f1e3cab45f57a34dfca4c2ce81416c5ec7c
Da-Va <david1valouch@gmail.com>
2022-02-09 13:54:29 +0100

    automatically create alias for dotfiles config git in .bashrc

--- system-init/dotfiles-init.sh
+++ system-init/dotfiles-init.sh
@@ -14,8 +14,16 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  mkdir -p .config-backup
-  config checkout 2>&1 | grep -E "\s+\." | awk \{'print $1'\} | xargs -I{} mv {} .config-backup/{}
+  mkdir -p .dotfiles-backup
+  config checkout 2>&1 | grep -E "\s+\." | awk \{'print $1'\} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+cat >> $HOME/.bashrc << EOF
+
+# alias for dotfiles git bare repository
+alias config=/usr/bin/git --git-dir="$LOCAL"/ --work-tree="$HOME" "$@"
+source /usr/share/bash-completion/completions/git
+__git_complete config __git_main
+EOF
