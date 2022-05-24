3c9479f7ee381f794b650cf7e45e1df7e13f4dbf
valoudav <david1valouch@gmail.com>
2022-05-24 16:15:02 +0200

    moved bin -> scripts

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 cd "$(dirname "$0")" || exit 1
-. setup/utils.sh
+. SETUP/utils.sh
 
 case "$1" in
-  uninstall)
-    stow_uninstall dotfiles "$HOME"
-    stow_uninstall bin "$HOME"
-    ;;
   install | '')
     stow_install dotfiles "$HOME"
-    stow_install bin "$HOME"
+    scripts/install.sh install
+    ;;
+  uninstall)
+    stow_uninstall dotfiles "$HOME"
+    scripts/install.sh uninstall
     ;;
   *)
-    echo "Unknown command "$1""
+    echo "Unknown command $1"
     ;;
 esac
